961580cf0ba5db809b191cee9d22ae7afdff30d6
Lazizbek <lazizbekabdullayev118@gmail.com>
2024-01-16 17:35:50 +0500

    asda

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
