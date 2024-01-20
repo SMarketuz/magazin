f9d2f6bfb07485f338a112d4b01cfef4182fac92
MOdevco <modevco05@gmail.com>
2024-01-20 17:11:23 +0500

    fds

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,19 @@ const antpoint = require('./server/antpoints')
 const cors = require('cors')
 const mongoose = require('mongoose');
 
-app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-require('./server/antpoints')(app)
-require('./server/db')()
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Headers","*")
+    res.header("Access-Control-Allow-Headers","Access-Control-Allow-Headers")
+    next()
+})
 app.use(cors({
     origin: '*',
     credentials: true,
     methods: ["GET" , "POST" , "PUT" , "DELETE"]
 }))
+require('./server/antpoints')(app)
+require('./server/db')()
+
 
 const port = process.env.PORT || 5000
 app.listen(port ,() => {
