eeef26ba3df6431f768eb0e54d062ef3236d0551
Lazizbek <lazizbekabdullayev118@gmail.com>
2024-01-25 14:39:49 +0500

    swippers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Box } from "@chakra-ui/react"
 import Register from "./components/Register"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero/Hero"
+import Top from "./components/Top/Top"
 function App() {
 
   return (
       <Box>
         <Navbar/>
         <Hero/>
+        <Top/>
       </Box>
   )
 }
