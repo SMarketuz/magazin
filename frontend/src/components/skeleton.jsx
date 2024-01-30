13adc17fb3746a811dce156b43f3da8c7e142ff9
Lazizbek <lazizbekabdullayev118@gmail.com>
2024-01-30 16:54:01 +0500

    dsfg

--- frontend/src/components/skeleton.jsx
+++ frontend/src/components/skeleton.jsx
@@ -1,23 +1,37 @@
 import { Box, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
 import React from 'react'
 
-export default function Skeleton() {
+export default function Skeleton({size}) {
     return (
-        <Box>
-            <Box padding='6' boxShadow='lg' bg='white'>
+        <Box display='flex' alignItems='center' justifyContent='space-between'>
+            <Box padding='6' boxShadow='lg' bg='white' width={size} height='350px'>
+                <SkeletonCircle size='10' />
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' /> 
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' /> 
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' /> 
+            </Box>
+            <Box padding='6' boxShadow='lg' bg='white' width={size} height='350px'>
                 <SkeletonCircle size='10' />
                 <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
-            </Box>
-            <Box padding='6' boxShadow='lg' bg='white'>
-                <SkeletonCircle size='10' />
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
                 <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
             </Box>
-            <Box padding='6' boxShadow='lg' bg='white'>
+            <Box padding='6' boxShadow='lg' bg='white' width={size} height='350px'>
                 <SkeletonCircle size='10' />
                 <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
             </Box>
-            <Box padding='6' boxShadow='lg' bg='white'>
+            <Box padding='6' boxShadow='lg' bg='white' width={size} height='350px'>
                 <SkeletonCircle size='10' />
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+            </Box>
+            <Box padding='6' boxShadow='lg' bg='white' width={size} height='350px'>
+                <SkeletonCircle size='10' />
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
                 <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
             </Box>
         </Box>
