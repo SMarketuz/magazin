b58c1252e36a18de3a64ab4649f6de4f1962a6f1
MOdevco <modevco05@gmail.com>
2024-01-30 17:35:05 +0500

    cds

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,3 @@
 // export const api = 'http://192.168.77.237:5000/'
 // export const api = 'https://47b9-95-214-211-190.ngrok-free.app/'
-export const api = 'https://2992-188-113-216-67.ngrok-free.app/'
+export const api = 'https://c6cc-95-214-210-45.ngrok-free.app/'
